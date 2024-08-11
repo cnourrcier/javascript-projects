c1ca0b3b8805511d2739352d1c4febaf24872109
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-08-11 12:13:09 -0400

    Update main page styles, add image slider to sidebar, remove fetch quote api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ****Load projects onto page start
 document.addEventListener('DOMContentLoaded', () => {
     fetch('./script/projectsData.json')
         .then(response => response.json())
@@ -82,141 +83,96 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// ****Load projects onto page end
 
 
 
+// ****Slideshow start
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
 
-
-// Fetch quotes from the API
-fetch("https://type.fit/api/quotes")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        // Fill _CONTENT with quotes from the API
-        _CONTENT = data.map(quote =>
-            `${quote.text}
-            - ${quote.author.split(',')[0]}`);
-        // Start the text animation
-        _INTERVAL_VAL = setInterval(Type, 100);
-    })
-    .catch(function (error) {
-        console.error("Error fetching quotes:", error);
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
     });
-
-// text animation
-let _CONTENT = [];
-
-let _PART = 0;
-let _PART_INDEX = 0;
-let _INTERVAL_VAL;
-const _ELEMENT = document.getElementById("text");
-const _CURSOR = document.getElementById("cursor");
-
-// typing effect
-function Type() {
-    let text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-    _ELEMENT.innerHTML = text;
-    _PART_INDEX++;
-
-    if (text === _CONTENT[_PART]) {
-        // hide the cursor
-        _CURSOR.style.display = 'none';
-        clearInterval(_INTERVAL_VAL);
-        setTimeout(function () {
-            _INTERVAL_VAL = setInterval(Delete, 50);
-        }, 1000);
-    }
 }
 
-// deleting effect
-function Delete() {
-    let text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-    _ELEMENT.innerHTML = text;
-    _PART_INDEX--;
-
-    if (text === '') {
-        clearInterval(_INTERVAL_VAL);
-
-        if (_PART == (_CONTENT.length - 1))
-            _PART = 0;
-        else
-            _PART++;
-
-        _PART_INDEX = 0;
-        setTimeout(function () {
-            _CURSOR.style.display = 'inline-block';
-            _INTERVAL_VAL = setInterval(Type, 100);
-        }, 200);
-    }
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
 }
 
+showSlide(currentSlide);
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
+// ****Slideshow end
 
 
-// const microservices = document.getElementById('microservices');
-// const games = document.getElementById('games');
-// const apiProjects = document.getElementById('api-projects');
 
-// const projectSections = [microservices, games, apiProjects];
 
-// // Toggle project Sections Start
-// const toggleMicroservicesHeader = document.getElementById('toggle-microservices');
-// const toggleGamesHeader = document.getElementById('toggle-games');
-// const toggleApiProjectsHeader = document.getElementById('toggle-api-projects');
 
-// const toggleMicroservicesLink = document.getElementById('nav-and-toggle-microservices');
-// const toggleGamesLink = document.getElementById('nav-and-toggle-games');
-// const toggleApiProjectsLink = document.getElementById('nav-and-toggle-api-projects');
 
-// const toggleSection = (section, header) => {
-//     const icon = header.querySelector('i');
-//     if (section.style.display === 'none' || section.style.display === '') {
-//         hideAllSections();
-//         icon.classList.add('up');
-//         section.style.display = 'grid';
-//     } else {
-//         section.style.display = 'none';
-//         icon.classList.remove('up');
+// // ****Fetch quotes from the API start
+// fetch("https://type.fit/api/quotes")
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         // Fill _CONTENT with quotes from the API
+//         _CONTENT = data.map(quote =>
+//             `${quote.text}
+//             - ${quote.author.split(',')[0]}`);
+//         // Start the text animation
+//         _INTERVAL_VAL = setInterval(Type, 100);
+//     })
+//     .catch(function (error) {
+//         console.error("Error fetching quotes:", error);
+//     });
+
+// // text animation
+// let _CONTENT = [];
+
+// let _PART = 0;
+// let _PART_INDEX = 0;
+// let _INTERVAL_VAL;
+// const _ELEMENT = document.getElementById("text");
+// const _CURSOR = document.getElementById("cursor");
+
+// // typing effect
+// function Type() {
+//     let text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+//     _ELEMENT.innerHTML = text;
+//     _PART_INDEX++;
+
+//     if (text === _CONTENT[_PART]) {
+//         // hide the cursor
+//         _CURSOR.style.display = 'none';
+//         clearInterval(_INTERVAL_VAL);
+//         setTimeout(function () {
+//             _INTERVAL_VAL = setInterval(Delete, 50);
+//         }, 1000);
 //     }
 // }
 
-// const hideAllSections = () => {
-//     projectSections.map(section => section.style.display = 'none');
-// }
+// // deleting effect
+// function Delete() {
+//     let text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+//     _ELEMENT.innerHTML = text;
+//     _PART_INDEX--;
 
-// toggleMicroservicesHeader.addEventListener('click', () => toggleSection(microservices, toggleMicroservicesHeader));
-// toggleGamesHeader.addEventListener('click', () => toggleSection(games, toggleGamesHeader));
-// toggleApiProjectsHeader.addEventListener('click', () => toggleSection(apiProjects, toggleApiProjectsHeader));
+//     if (text === '') {
+//         clearInterval(_INTERVAL_VAL);
 
-// toggleMicroservicesLink.addEventListener('click', () => toggleSection(microservices, toggleMicroservicesHeader));
-// toggleGamesLink.addEventListener('click', () => toggleSection(games, toggleGamesHeader));
-// toggleApiProjectsLink.addEventListener('click', () => toggleSection(apiProjects, toggleApiProjectsHeader));
+//         if (_PART == (_CONTENT.length - 1))
+//             _PART = 0;
+//         else
+//             _PART++;
 
-// // Toggle Projects Section End
-
-
-// // Fetch Projects Start
-// document.addEventListener('DOMContentLoaded', () => {
-//     fetch('./script/projectsData.json')
-//         .then(response => response.json())
-//         .then(data => {
-//             populateProjects('microservices', data.microservices);
-//             populateProjects('games', data.games);
-//             populateProjects('api-projects', data.apiProjects);
-//         });
-// });
-
-// function populateProjects(sectionId, projects) {
-//     const section = document.getElementById(sectionId);
-//     if (section && projects.length > 0) {
-//         section.innerHTML = projects.map(project => `
-//             <a href="${project.link}">
-//                 <figure class="project">
-//                     <img src="${project.img}" alt="${project.name}">
-//                     <p>${project.description}</p>
-//                 </figure>
-//             </a>
-//             `).join('');
+//         _PART_INDEX = 0;
+//         setTimeout(function () {
+//             _CURSOR.style.display = 'inline-block';
+//             _INTERVAL_VAL = setInterval(Type, 100);
+//         }, 200);
 //     }
 // }
-
+// // ****Fetch quotes from API end
